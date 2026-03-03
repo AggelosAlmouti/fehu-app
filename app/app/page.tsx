71009f38be0968244d3fda5b76a9d5af88248a7c
Aggelos <aggelos.almouti@gmail.com>
2026-03-03 16:54:21 +0200

    Use css modules instead of tailwind and init app page

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function App() {
+    return (
+        <div className={styles.container}>
+            <h1>Finance App</h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
+import styles from './page.module.css';
+import design from '@/public/design.png';
 
 export default function Home(){
   return (
-    <main className="min-h-screen flex flex-col gap-1 items-center justify-center">
-      <h1 className="text-xl font-bold">Coming Soon...</h1>
-        <Image 
-          src="/design.png" 
-          alt="Fehu sheep logo" 
-          width={300} 
-          height={300}
-        />
+    <main className={styles.main}>
+      <h1 className={styles.title}>Coming Soon...</h1>
+      <Image 
+        src={design}
+        alt="App design" 
+        style={{width: '300px', height: 'auto'}}
+      />
     </main>
   )
 }
