d4757caadf1ea751770b2bddf37f6217ded509cd
almouti <almouti@open.gr>
2026-03-05 12:33:00 +0200

    Study react

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,31 @@
 import styles from './page.module.css'
 
-export default function App() {
+
+function List(props) {
+    if (!props.animals) {
+        return <div>Loading...</div>
+    }
+
+    if (props.animals.length === 0) {
+        return <div>There are no animals in the list!</div>
+    }
+
     return (
-        <div className={styles.container}>
-            <h1>Finance App</h1>
+        <ul>
+            {props.animals.map((animal) => {
+                return <li key={animal}>{animal}</li>
+            })}
+        </ul>
+    )
+}
+
+export default function App() {
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+    return (
+        <div>
+            <h1>Animals: </h1>
+            <List animals={animals} />
         </div>
     )
 }
