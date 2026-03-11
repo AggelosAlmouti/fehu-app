787b10a470adb78a8c207804d5359e054002e10e
almouti <almouti@open.gr>
2026-03-11 13:36:44 +0200

    Test iphone top and bottom margins

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function App() {
+  const [activeTab, setActiveTab] = useState("Spending");
+
   return (
-    <main className="main">
-      <h1>hello</h1>
+    <main className={styles.main}>
+      <div className={styles.tabs}>
+        <button 
+        className={`${styles.tab} ${activeTab === "spending" ? styles.activeTab : ""}`}
+        onClick={() => setActiveTab("spending")}>Spending</button>
+        <button 
+        className={`${styles.tab} ${activeTab === "income" ? styles.activeTab : ""}`}
+        onClick={() => setActiveTab("income")}>Income</button>
+      </div>
     </main>
   );
 }
